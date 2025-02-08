4c7a76897c8e5d4c96d87d328c415cbdb7e75b75
RajaPrajapati001 <rajaprajapati3454@gmail.com>
2025-02-08 13:33:43 +0530

    no changes

--- CurrentTime.java
+++ CurrentTime.java
@@ -4,6 +4,5 @@ public class CurrentTime {
     public static void main(String[] args) {
         LocalTime time = LocalTime.now();
         System.out.println("Current Time: " + time);
-        System.out.println("Hello");
     }
 }
